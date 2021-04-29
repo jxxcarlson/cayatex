1baa0f04d5ba302289c6144deb0910c71782fac0
James Carlson <jxxcarlson@gmail.com>
2021-04-29 13:06:47 -0400

    Add: UpdateDataTests

--- tests/UpdateDataTests.elm
+++ tests/UpdateDataTests.elm
@@ -2,9 +2,9 @@ module UpdateDataTests exposing (..)
 
 import Dict
 import Expect
-import Parser.Data as Data
+import Parser.Data as Data exposing (Data)
 import Parser.Driver as Driver
-import Parser.Element exposing (..)
+import Parser.Element as Element exposing (Element)
 import Test exposing (describe, fuzz, test)
 
 
@@ -20,10 +20,20 @@ empty =
 suite =
     describe "Parser.Data"
         [ describe "update"
-            [ test "two sections" <|
+            [ test "two sections from element" <|
                 \_ ->
+                    let
+                        updateCounters : Element -> Data
+                        updateCounters e =
+                            Data.update e empty
+                    in
                     Expect.equal
-                        (Data.updateList (pl "[section A]\n\n[section B]") empty |> .counters)
-                        (Dict.fromList [ ( "section1", 2 ) ])
+                        (Result.map (Element.makeList >> updateCounters >> .counters) (Element.parseList 0 0 "[section A]\n\n[section B]"))
+                        (Ok <| Dict.fromList [ ( "section1", 2 ) ])
             ]
+        , test "two sections from element list" <|
+            \_ ->
+                Expect.equal
+                    (Data.updateList (pl "[section A]\n\n[section B]") empty |> .counters)
+                    (Dict.fromList [ ( "section1", 2 ) ])
         ]
