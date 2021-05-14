8cdc37ead03a07aeafbba043c9d3e0c7f124a064
James Carlson <jxxcarlson@gmail.com>
2021-05-13 21:39:10 -0400

    reformat

--- app/public/assets/elm-katex.js
+++ app/public/assets/elm-katex.js
@@ -11,33 +11,33 @@ const init =  async function(app) {
 
 }
 
-function initKatex() {
+    function initKatex() {
 
-  console.log("elm-katex: initializing");
+      console.log("elm-katex: initializing");
 
-  class MathText extends HTMLElement {
+      class MathText extends HTMLElement {
 
-     constructor() {
-         // Always call super first in constructor
-         super();
-       }
+         constructor() {
+             // Always call super first in constructor
+             super();
+           }
 
-    connectedCallback() {
-      this.attachShadow({mode: "open"});
-      this.shadowRoot.innerHTML =
-        katex.renderToString(
-          this.content,
-          { throwOnError: false, displayMode: this.display }
-        );
-      let link = document.createElement('link');
-      link.setAttribute('rel', 'stylesheet');
-      link.setAttribute('href', 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css');
-      this.shadowRoot.appendChild(link);
+        connectedCallback() {
+          this.attachShadow({mode: "open"});
+          this.shadowRoot.innerHTML =
+            katex.renderToString(
+              this.content,
+              { throwOnError: false, displayMode: this.display }
+            );
+          let link = document.createElement('link');
+          link.setAttribute('rel', 'stylesheet');
+          link.setAttribute('href', 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css');
+          this.shadowRoot.appendChild(link);
+
+        }
+
+      }
+
+      customElements.define('math-text', MathText)
 
     }
-
-  }
-
-  customElements.define('math-text', MathText)
-
-}
